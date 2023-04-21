320b32ccd048ca69487711c7f227e6b0c23f45bf
Dmitriy <999gothicman999@mail.ru>
2023-04-21 23:02:01 +0700

    Add hh_api

--- hh_api_homework.py
+++ hh_api_homework.py
@@ -3,14 +3,28 @@ import pprint
 
 url = 'https://api.hh.ru/vacancies'
 
-params = {'text': 'Python', 'area': 1}
+params = {'text': 'NAME:(Python)', 'area': 1}
 
 result = requests.get(url, params=params).json()
 
-num_of_vac = result['found']
+# Всего найдено вакансий
+total_vac = result['found']
+print(f'Всего вакансий по запросу Python: {total_vac}')
 
-print(num_of_vac)
+# Всего страниц с вакансиями
+total_pages = result['pages']
+print(f'Всего страниц по запросу Python: {total_pages}')
 
-print(result['items'])
+vac_json = []
+for i in range(total_pages):
+    vac_json.append(requests.get(url, params={'text': 'NAME:(Python)', 'area': 1, 'page': i, 'per_page': 20}).json())
 
-#pprint.pprint(result)
+pprint.pprint(vac_json[30])
+
+
+#salary = []
+#for i in vacancies:
+#    salary.append(i['salary'])
+
+#print(salary)
+#pprint.pprint(vacancies)
