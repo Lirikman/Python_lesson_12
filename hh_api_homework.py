1dca0f3b4d03fdaa6073c5ba57cc7bc1348c2262
Dmitriy <999gothicman999@mail.ru>
2023-04-23 00:28:14 +0700

    Add average_salary

--- hh_api_homework.py
+++ hh_api_homework.py
@@ -15,16 +15,33 @@ print(f'Всего вакансий по запросу Python: {total_vac}')
 total_pages = result['pages']
 print(f'Всего страниц по запросу Python: {total_pages}')
 
+# Объект для хранения скаченных вакансий с HH.RU по запросу Python
 vac_json = []
+
+# Скачиваем вакансии в названии которых содержится слово Python
 for i in range(total_pages):
-    vac_json.append(requests.get(url, params={'text': 'NAME:(Python)', 'area': 1, 'page': i, 'per_page': 20}).json())
+    url = 'https://api.hh.ru/vacancies'
+    params = {'text': 'NAME:(Python)', 'area': 1, 'page': i, 'per_page': 20}
+    vac_json.append(requests.get(url, params=params).json())
 
-pprint.pprint(vac_json[30])
+# Анализируем полученную информацию
+all_salary = 0
+all_vac = 0
+for i in vac_json:
+    items = i['items']
+    count_vac = 0
+    summ_salary = 0
+    for j in items:
+        if j['salary'] is not None:
+            s = j['salary']
+            if s['from'] is not None:
+                count_vac += 1
+                summ_salary += s['from']
+    all_salary += summ_salary
+    all_vac += count_vac
+
+average_salary = all_salary//all_vac
+print(f'Средняя заработная плата по запросу Python в Москве: {average_salary} руб.')
 
 
-#salary = []
-#for i in vacancies:
-#    salary.append(i['salary'])
-
-#print(salary)
-#pprint.pprint(vacancies)
+#Cчетчик = 11
